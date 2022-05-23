e53c2fc1f119b96f2807f22d31d142fc0b348528
Chhaya Kumar Das <chayakudas.das56@gmail.com>
2022-05-23 13:47:19 +0530

    Pool

---
+++ pool/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var numCalcsCreated int
+	calcPool := &sync.Pool{
+		New: func() interface{} {
+			numCalcsCreated += 1
+			mem := make([]byte, 1024)
+			return &mem
+		},
+	}
+
+	fmt.Println("calcPool.New", calcPool.New())
+	const numWorkers = 1024 * 1024
+	var wg sync.WaitGroup
+	wg.Add(numWorkers)
+
+	for i := numWorkers; i > 0; i-- {
+		go func() {
+			defer wg.Done()
+			mem := calcPool.Get().(*[]byte)
+			defer calcPool.Put(mem)
+		}()
+	}
+
+	wg.Wait()
+	fmt.Println("Calculators created:", numCalcsCreated)
+}
